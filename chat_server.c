ac93bc3d08183c74a1dfeffa8ece9da7578febb6
rope-park <eulmidesign@gmail.com>
2025-05-25 15:16:52 +0900

    Refactor: chat_server.c 구조 리팩터링 - chat_server.c: 서버 명령어 테이블(cmd_tbl_server)에 'recent_users' 명령어process_server_cmd()에 db_recent_user() 부분 추가

--- chat_server.c
+++ chat_server.c
@@ -13,151 +13,33 @@
 #include <sys/time.h>
 #include <time.h>
 #include <ctype.h>
+#include "chat_server.h"
 #include "db_helper.h" // 데이터베이스 관련 함수들
 
-#define PORTNUM         9000
-#define MAX_CLIENT      100
-#define BUFFER_SIZE     1024
-
-
-// User 구조체
-typedef struct User {
-    int sock;                           // 소켓 번호
-    char id[20];                        // 사용자 ID (닉네임)
-    pthread_t thread;                   // 사용자별 스레드
-    struct Room *room;                  // 대화방 포인터
-    struct User *next;                  // 다음 사용자 포인터
-    struct User *prev;                  // 이전 사용자 포인터
-    struct User *room_user_next;        // 대화방 내 사용자 포인터
-    struct User *room_user_prev;        // 대화방 내 사용자 포인터
-} User;
-
-// Room 구조체
-typedef struct Room {
-    char room_name[32];                 // 방 이름
-    unsigned int no;                    // 방 고유 번호
-    User *manager;                      // 방장
-    User *members[MAX_CLIENT];          // 방에 참여중인 멤버 목록
-    int member_count;                   // 생에 참여중인 멤버 수
-    struct Room *next;                  // 다음 방 포인터
-    struct Room *prev;                  // 이전 방 포인터
-    time_t created_time;                // 생성된 시간
-} Room;
-
-
-// ================== 전역 변수 ===================
-static User *g_users = NULL; // 사용자 목록
-static Room *g_rooms = NULL; // 대화방 목록
-static int g_server_sock = -1; // 서버 소켓
-static int g_epfd = -1; // epoll 디스크립터
-static unsigned int g_next_room_no = 1; // 다음 대화방 고유 번호
+// ================== 전역 변수 초기화 ===================
+User *g_users = NULL; // 사용자 목록
+Room *g_rooms = NULL; // 대화방 목록
+int g_server_sock = -1; // 서버 소켓
+int g_epfd = -1; // epoll 디스크립터
+unsigned int g_next_room_no = 1; // 다음 대화방 고유 번호
 
 // Mutex 사용하여 스레드 상호 배제를 통해 안전하게 처리
 pthread_mutex_t g_users_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t g_rooms_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-
-// ==================== 기능 함수 선언 =====================
-// ======== 서버부 ========
-// ==== CLI ====
-void server_user(void);             // users 명령: 사용자 목록
-void server_room(void);             // rooms 명령: 대화방 목록
-void server_quit(void);             // quit 명령: 서버 종료
-// ==== 메시지 전송 ====
-ssize_t safe_send(int sock, const char *msg);
-void broadcast_to_room(Room *room, User *sender, const char *format, ...);
-void broadcast_to_all(User *sender, const char *format, ...);
-// ==== 리스트 및 방 관리 ====
-void list_add_client_unlocked(User *user);
-void list_remove_client_unlocked(User *user);
-User *find_client_by_sock_unlocked(int sock);
-User *find_client_by_id_unlocked(const char *id);
-
-void list_add_room_unlocked(Room *room);
-void list_remove_room_unlocked(Room *room);
-Room *find_room_unlocked(const char *name);
-Room *find_room_by_id_unlocked(unsigned int id);
-
-void room_add_member_unlocked(Room *room, User *user);
-void room_remove_member_unlocked(Room *room, User *user);
-void destroy_room_if_empty_unlocked(Room *room);
-// ==== 동기화(Mutex) 래퍼 ====
-void list_add_client(User *user);
-void list_remove_client(User *user);
-User *find_client_by_sock(int sock);
-User *find_client_by_id(const char *id);
-
-void list_add_room(Room *room);
-void list_remove_room(Room *room);
-Room *find_room(const char *name);
-Room *find_room_by_id(unsigned int id);
-
-void room_add_member(Room *room, User *user);
-void room_remove_member(Room *room, User *user);
-void destroy_room_if_empty(Room *room);
-// ==== 세션 처리 ====
-void *client_process(void *args);
-void process_server_cmd(int epfd, int server_sock);
-
-// ======== 클라이언트부 ========
-// ==== CLI ====
-void cmd_users(User *user);
-void cmd_users_wrapper(User *user, char *args);
-void cmd_rooms(int sock);
-void cmd_rooms_wrapper(User *user, char *args);
-void cmd_id(User *user, char *args);
-void cmd_manager(User *user, char *user_id);
-void cmd_change(User *user, char *room_name);
-void cmd_kick(User *user, char *user_id);
-void cmd_create(User *creator, char *room_name);
-void cmd_join(User *user, char *room_no_str);
-void cmd_join_wrapper(User *user, char *args);
-void cmd_leave(User *user);
-void cmd_leave_wrapper(User *user, char *args);
-void cmd_help(User *user);
-void cmd_help_wrapper(User *user, char *args);
-
-void usage_id(User *user);
-void usage_manager(User *user);
-void usage_change(User *user);
-void usage_kick(User *user);
-void usage_create(User *user);
-void usage_join(User *user);
-void usage_leave(User *user);
-void usage_help(User *user);
-
-
-// 서버 cmd 구조체
-typedef void (*server_cmd_func_t)(void);
-typedef struct {
-    const char          *cmd;       // 명령어 문자열
-    server_cmd_func_t   cmd_func;   // 해당 명령어 처리 함수
-    const char          *comment;   // 도움말 출력용 설명
-} server_cmd_t;
-
 // command 테이블 (서버)
 server_cmd_t cmd_tbl_server[] = {
     {"users", server_user, "Show all users"},
+    {"recent_users", server_user, "Show recent users"},
     {"rooms", server_room, "Show all chatrooms"},
     {"quit", server_quit, "Quit server"},
     {NULL, NULL, NULL},
 };
 
-// 클라이언트 cmd 구조체
-typedef void (*cmd_func_t)(User *user, char *args);
-typedef void (*usage_func_t)(User *user);
-typedef struct {
-    const char          *cmd;            // 명령어 문자열
-    cmd_func_t          cmd_func;        // 해당 명령어 처리 함수
-    usage_func_t        usage_func;      // 사용법 안내 함수
-    const char          *comment;        // 도움말 출력용 설명
-} client_cmd_t;
-
-
 // command 테이블 (클라이언트)
 client_cmd_t cmd_tbl_client[] = {
-    {"/users", cmd_users_wrapper, NULL, "List all users"},
-    {"/rooms", cmd_rooms_wrapper, NULL, "List all chatrooms"},
+    {"/users", cmd_users_wrapper, usage_help, "List all users"},
+    {"/rooms", cmd_rooms_wrapper, usage_help, "List all chatrooms"},
     {"/id", cmd_id, usage_id, "Change your ID(nickname)"},
     {"/manager", cmd_manager, usage_manager, "Change room manager"},
     {"/change", cmd_change, usage_change, "Change room name"},
@@ -168,7 +50,6 @@ client_cmd_t cmd_tbl_client[] = {
     {"/help", cmd_help_wrapper, usage_help, "Show all available commands"},
     {NULL, NULL, NULL, NULL},
 };
-
 
 // ==================== 기능 함수 구현 ======================
 // ======== 서버부 ========
@@ -448,6 +329,7 @@ Room *find_room_unlocked(const char *name) {
         }
         current = current->next;
     }
+    return NULL;
 }
 
 // 대화방 ID로 검색 함수
@@ -812,8 +694,8 @@ void *client_process(void *args) {
         shutdown(user->sock, SHUT_RDWR);
         close(user->sock);
     }
-    free(user);
     db_disconnect_user(user); // 데이터베이스 연결 해제
+    free(user);
     pthread_exit(NULL);
 }
 
@@ -831,35 +713,41 @@ void process_server_cmd(int epfd, int server_sock) {
     // 개행 문자 제거
     cmd_buf[strcspn(cmd_buf, "\r\n")] = '\0';
 
-    if (strncmp(cmd_buf, "recent_users", 12) == 0) {
-        // 데이터베이스로부터 최근 사용자 목록 출력
-        int limit = atoi(cmd_buf + 11);
-        db_recent_user(limit);
-    }
+    char *cmd = strtok(cmd_buf, " "); // 첫 번째 토큰을 명령어로 사용
+    char *arg = strtok(NULL, ""); // 나머지 토큰을 인자로 사용
 
-    // 명령어와 인자 분리
-    char *cmd = strtok(cmd_buf, " ");
-
-    // 인자 처리 - 명령어가 없거나 빈 문자열인 경우
     if (!cmd || strlen(cmd) == 0) {
-        printf("Bad server command.\n");
+        printf("No command entered. Available: users, rooms, recent_users, quit\n");
         fflush(stdout); // 버퍼 비우기
         return;
     }
 
-    // cmd 테이블을 순회하며 명령어 처리
-    for (int i = 0; cmd_tbl_server[i].cmd != NULL; i++) {
-        if (strcmp(cmd, cmd_tbl_server[i].cmd) == 0) {
-            cmd_tbl_server[i].cmd_func();
-            printf("> ");
-            fflush(stdout);
-            return;
-        }
+    if (strcmp(cmd, "users") == 0) {
+        server_user();
     }
-
-    // 명령어가 테이블에 없는 경우
-    printf("Unknown server command: %s. Available: users, rooms, quit\n", cmd);
-    fflush(stdout);
+    else if (strcmp(cmd, "rooms") == 0) {
+        server_room();
+    }
+    else if (strcmp(cmd, "recent_users") == 0) {
+        // 데이터베이스로부터 최근 사용자 목록 출력
+        int limit = 10; // 기본값 10
+        if (arg) {
+            limit = atoi(arg);
+            if (limit <= 0) {
+                printf("Limit must be a positive integer.\n");
+                fflush(stdout); // 버퍼 비우기
+                return;
+            }
+        }
+        db_recent_user(limit);
+    }
+    else {
+        printf("Unknown server command: %s. Available: users, rooms, recent_users, quit\n", cmd_buf);
+        fflush(stdout); // 버퍼 비우기
+        return;
+    }
+    printf("> ");
+    fflush(stdout); // 버퍼 비우기
 }
 
 
@@ -902,7 +790,7 @@ void cmd_users(User *user) {
         }
         pthread_mutex_unlock(&g_users_mutex);
     }
-    db_recent_user(user_list); // 최근 사용자 목록 업데이트
+    db_recent_user(10); // 최근 사용자 목록 업데이트
     
     strcat(user_list, "\n");
     safe_send(user->sock, user_list);
@@ -1277,7 +1165,6 @@ void cmd_join(User *user, char *room_no_str) {
 
 // typedef에서 warning: type allocation error 방지
 void cmd_join_wrapper(User *user, char *args) {
-    char buf[256];
     if (!args) {
         usage_join(user);
         return;
