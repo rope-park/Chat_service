b028c868d898ff3855053bfc5a17e883634c410a
rope-park <kelly6789@naver.com>
2025-05-27 23:22:51 +0900

    Feat: 추가된 기능 헤더 처리 - server_user_info_wrapper(), server_room_info(), server_room_info_wrapper(), cmd_delete_message(), usage_delete_account(), usage_delete_message() 추가

--- chat_server.h
+++ chat_server.h
@@ -55,7 +55,9 @@ extern pthread_mutex_t g_db_mutex;
 // ==== CLI ====
 void server_user(void);                             // users 명령: 사용자 목록
 void server_user_info(User *user, char *id);        // user_info 명령: 사용자 정보 출력
-void serveR_user_info_wrapper(void);                // user_info 명령 래퍼
+void server_user_info_wrapper(void);                // user_info 명령 래퍼
+void server_room_info(User *user, char *room_name); // room_info 명령: 대화방 정보 출력
+void server_room_info_wrapper(void);                // room_info 명령 래퍼
 void server_room(void);                             // rooms 명령: 대화방 목록
 void server_quit(void);                             // quit 명령: 서버 종료
 // ==== 메시지 전송 ====
